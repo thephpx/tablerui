9fd61cc86eff34e1ae1f5184f4c821ca22497e1c
Faisal Ahmed <thephpx@gmail.com>
2024-06-08 21:49:15 +0600

    composer file and service provider class added

---
+++ src/Providers/TableruiServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Thephpx\Tablerui\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class TableruiServiceProvider extends ServiceProvider
+{
+  public function register()
+  {
+    //
+  }
+
+  public function boot()
+  {
+    # Load model relations 
+    
+    # Load routes
+    $this->loadRoutesFrom(__DIR__.'/../../routes/web.php');
+
+    # Load views
+    $this->loadViewsFrom(__DIR__.'/../../resources/views', 'Tablerui');
+
+    # Load migration tables
+    $this->loadMigrationsFrom(__DIR__ . '/../../database/migrations');
+
+    # Publish assets
+    // $this->publishes([
+    //   __DIR__.'/../../config/dummy.php' => config_path('dummy.php'),
+    // ], 'config');
+  }
+}
