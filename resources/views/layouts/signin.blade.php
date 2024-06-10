b25e02461afc0140ad4a8ded218e72c10928f2d6
Faisal Ahmed <thephpx@gmail.com>
2024-06-10 16:16:37 +0600

    new layout added

---
+++ resources/views/layouts/signin.blade.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
+  <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+  <title>Blank page - Tabler - Premium and Open Source dashboard template with responsive and high quality UI.</title>
+  <!-- CSS files -->
+  <link href="{{ asset('assets/tablerui/css/tabler.min.css') }}" rel="stylesheet" />
+  <link href="{{ asset('assets/tablerui/css/tabler-flags.min') }}" rel="stylesheet" />
+  <link href="{{ asset('assets/tablerui/css/tabler-payments.min') }}" rel="stylesheet" />
+  <link href="{{ asset('assets/tablerui/css/tabler-vendors.min') }}" rel="stylesheet" />
+  <style>
+    @import url('https://rsms.me/inter/inter.css');
+
+    :root {
+      --tblr-font-sans-serif: 'Inter Var', -apple-system, BlinkMacSystemFont, San Francisco, Segoe UI, Roboto, Helvetica Neue, sans-serif;
+    }
+
+    body {
+      font-feature-settings: "cv03", "cv04", "cv11";
+    }
+  </style>
+</head>
+
+<body class="d-flex flex-column">
+  <div class="page page-center">
+    
+    @yield('content')
+
+  </div>
+  <!-- Tabler Core -->
+  <script src="{{ asset('assets/tablerui/js/tabler.min.js') }}" defer></script>
+</body>
+</html>
