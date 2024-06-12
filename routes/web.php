0c478a068f53e5381ce367735bd5fb6e1c631dee
Faisal Ahmed <thephpx@gmail.com>
2024-06-12 23:22:11 +0600

    updated route

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('tablerui')->middleware(['web'])->group(function () {
+Route::prefix('tablerui')->middleware(['web','auth'])->group(function () {
 	Route::get('/blank', \Thephpx\Tablerui\Http\Controllers\Index::class)->name('tablerui.blank');
 	Route::get('/form', \Thephpx\Tablerui\Http\Controllers\Form::class)->name('tablerui.form');
 });
